3c9dee5d7300562d2dcedf3f27677fe8237b2f31
Yandr <102684712+Yandr2022@users.noreply.github.com>
2022-10-04 17:52:38 +0300

    enum & SingleTon lesson 3005

--- src/itStep/yandr/javaLessons/lesson39/EnumSeason.java
+++ src/itStep/yandr/javaLessons/lesson39/EnumSeason.java
@@ -1,5 +1,22 @@
 package itStep.yandr.javaLessons.lesson39;
 
+enum DayOfWeek {
+    MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY
+}
+
+enum FlowerColor{
+    RED, BLUE, GREEN, YELLOW
+}
+
+enum Killer {
+    KILLER;
+
+    public void kill() {
+
+    }
+
+}
+
 enum EnumSeason {
 
     WINTER("Winter",1), SPRING("Spring",2), SUMMER("Summer",3), AUTUMN("Autumn",4);
--- src/itStep/yandr/javaLessons/lesson39/Main.java
+++ src/itStep/yandr/javaLessons/lesson39/Main.java
@@ -2,14 +2,41 @@ package itStep.yandr.javaLessons.lesson39;
 
 public class Main {
     public static void main(String[] args) {
-        Season s1 = Season.WINTER;
-        Season s2 = Season.SPRING;
-        Season s3 = Season.SUMMER;
-        Season s4 = Season.AUTUMN;
-        System.out.println(s1);
-        System.out.println(s2);
-        System.out.println(s3);
-        System.out.println(s4);
+//        EnumSeason s1 = EnumSeason.WINTER;
+//        EnumSeason s2 = EnumSeason.SPRING;
+//        EnumSeason s3 = EnumSeason.SUMMER;
+//        EnumSeason s4 = EnumSeason.AUTUMN;
+//        DayOfWeek day = DayOfWeek.MONDAY;
+//        System.out.println(s1);
+//        System.out.println(s2);
+//        System.out.println(s3);
+//        System.out.println(s4);
+
+//        FlowerColor color = FlowerColor.BLUE;
+//        System.out.println(color);
+//        System.out.println(color.ordinal());
+//         String name = "Red";
+//        for (FlowerColor color:FlowerColor.values()) {
+//            if (name.equals(color.name())) {
+//                FlowerColor c = FlowerColor.valueOf(name);
+//                System.out.println(c);
+//            }
+//        }
+
+        EnumSeason season = null;
+        switch (season) {
+            case WINTER:
+                System.out.println(0);break;
+            case SPRING:
+                System.out.println(1);break;
+            case SUMMER:
+                System.out.println(2);break;
+            case AUTUMN:
+                System.out.println(3);break;
+            default:
+                throw new IllegalStateException("Unexpected value: " + season);
+        }
+
 
     }
 }
